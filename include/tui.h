a3116b2a2400588a72b62d0aed5c38eefb21ef0b
Subeen Regmi <subeenregmi123@gmail.com>
2024-09-17 13:02:31 +0100

    work on a better tui

--- include/tui.h
+++ include/tui.h
@@ -3,15 +3,67 @@
 
 #include <sys/ioctl.h>
 
-#include "messages.h"
+#define GOTOXY(x,y) printf("\033[%d;%dH", (y), (x))
 
-#define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
+#define HORIZONTAL_BORDER   '-'
+#define VERTICAL_BORDER     '|'
+#define CORNER_BORDER       '+'
+#define INPUT_CHARACTER     '>'
+
+#define MAX_CONTAINER_COMPONENTS 64
+
+enum c_type {
+    container = 0,
+    text      = 1,
+    button    = 2,
+    input_box = 3,
+};
+
+enum c_options {
+    disabled  = 0b00001,
+    hover     = 0b00010,
+    scroll    = 0b00100,
+    expand    = 0b01000,
+    hidden    = 0b10000,
+};
+
+struct c_position {
+    int s_x;
+    int s_y;
+    int e_x;
+    int e_y;
+};
+
+struct c_text {
+    char *msg;
+};
+
+struct c_button {
+    char *msg;
+    void (*on_submit)();
+};
+
+struct c_input_box {
+    char *msg;
+    char *output;
+    void (*on_submit)(char *);
+};
+
+struct component {
+    enum c_type type;
+    struct c_position pos;
+    unsigned int opt;
+    union {
+        struct c_text text;
+        struct c_button button;
+        struct c_input_box input;
+        struct component *container[MAX_CONTAINER_COMPONENTS];
+    };
+};
 
 struct winsize get_window_size();
 void clear_screen();
-void display_title(struct winsize *w, char *title);
-void display_user_input(struct winsize *w, char *buffer);
-void display_messages(struct winsize *w, struct message *tail);
-void handle_user_input(struct winsize *w, char *msg);
+
+void draw_component(struct component *c);
 
 #endif
--- src/tui.c
+++ src/tui.c
@@ -1,18 +1,9 @@
-#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h> 
 #include <sys/ioctl.h>
 
-#include "messages.h"
 #include "tui.h"
-
-#define HOR_BORDER '-'
-#define VERT_BORDER '|'
-#define CORNER_BORDER '+'
-
-#define INPUT_CHAR '>'
 
 void clear_screen(){
 	printf("\e[1;1H\e[2J");
@@ -24,168 +15,23 @@ struct winsize get_window_size(){
 	return w;
 }
 
-void display_title(struct winsize *w, char *title){
-
-	for(int i=0; i<w->ws_col; i++){
-		printf("%c", HOR_BORDER);
-	}
-	printf("\n");
-
-	int padding = (w->ws_col - strlen(title))/2;
-	printf("%c", VERT_BORDER);
-	for(int i=0; i<padding-1; i++){
-		printf(" ");
-	}
-	printf("%s", title);
-	for(int i=0; i<padding; i++){
-		printf(" ");
-	}
-	printf("%c\n", VERT_BORDER);
-
-	for(int i=0; i<w->ws_col; i++){
-		printf("%c", HOR_BORDER);
-	}
-	printf("\n");
-}
-
-void display_user_input(struct winsize *w, char *buffer) {
-	for(int i=0; i<w->ws_col; i++){
-		printf("%c", HOR_BORDER);
-	}
-	printf("\n");
-
-	printf("%c", VERT_BORDER);
-
-	int usedspace = 0;
-	printf(" %c", INPUT_CHAR);
-	if (strlen(buffer) > 0) {
-		usedspace = strlen(buffer)+1;
-		printf(" %s", buffer);
-	}
-
-	for(int i=0; i<w->ws_col-4-usedspace; i++){
-		printf(" ");
-	}
-
-	printf("%c", VERT_BORDER);
-
-	for(int i=0; i<w->ws_col; i++){
-		printf("%c", HOR_BORDER);
-	}
-}
-
-void display_messages(struct winsize *w, struct message *msg_tail){
-	int available_lines= w->ws_row - 6;
-	if (msg_tail == NULL) {
-		for(int i=0; i<available_lines; i++){
-			printf("\n");
-		}
-		return;
-	}
-
-	struct message *node = msg_tail;
-
-	while(available_lines > 0){
-		char *message;
-		sprintf(message, "%s: %s\n", node->user, node->message);
-		int lines_to_take = (strlen(message) / w->ws_col) + 1;
-
-		if((available_lines - lines_to_take) <= 0){
-			break;
-		}
-		if(node->left == NULL){
-			break;
-		}
-		if(node->left->left == NULL) {
-			break;
-		}
-		available_lines -= lines_to_take;
-		node = node->left;
-	
-	}
-	while(node != NULL){
-		printf("%s: %s\n", node->user, node->message);
-		node = node->right;
-
-	}
-	for(int i=0; i<available_lines-1; i++){
-		printf("\n");
-	}
-
-}
-
-void handle_user_input(struct winsize *w, char *msg){
-	int user_x = 5;
-	int user_y = w->ws_row - 1;
-	gotoxy(user_x, user_y);
-
-	char c;
-	int i=0;
-	while((c = getchar()) != '\n') {
-		if (c == 127) {
-			i--;
-			if (i < 0) {
-				i = 0;
-				msg[0] = '\0';
-				continue;
-			}
-			printf("\b \b");
-			msg[i] = '\0';
-			continue;
-		}
-		if (isprint(c) == 0) {
-			continue;	
-		}
-		putchar(c);
-		msg[i] = c;
-		i++;
-	}
-	msg[i++] = '\0';
-}
-
-void display_main_menu(struct winsize *w, char *message, char *name, char *server) {
-	int len = strlen(message);
-	for	(int j=0; j<w->ws_row - 1; j++) {
-		for (int i=0; i<w->ws_col; i++) {
-			if ((i == 0 && j == 0) ||
-				(i == w->ws_col - 1 && j == 0) ||
-				(i == w->ws_col - 1 && j == w->ws_row -2) ||
-				(i == 0 && j == w->ws_row -2)) 
-			{
-				putc(CORNER_BORDER, stdout);	
-			}
-			else if (j == 0 || j == w->ws_row - 2) {
-				putc(HOR_BORDER, stdout);
-			}
-			else if (i == 0 || i == w->ws_col - 1){
-				putc(VERT_BORDER, stdout);
-			}
-			else if (j == (w->ws_row / 2) - 5 && i == (w->ws_col - len)/ 2) {
-				printf("%s", message);
-				i += len;
-				printf(" ");
-			}
-			else {
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}
-}
-
-void display_main_menu_buttons(struct winsize *w) {
+void draw_component(struct component *c) {
 	
 }
 
 int main(void) {
 	struct winsize w = get_window_size();
-	
-	char *name = calloc(sizeof(char), 256);
-	char *server = calloc(sizeof(char), 256);
-	char *message = "Welcome to speak2me!";
-
-	display_main_menu(&w, message, name, server);
-	display_main_menu_buttons(&w);
-	free(name);
-	free(server);
+	struct component screen = {
+		.type = container,
+		.pos = {
+			.s_x = 0,
+			.s_y = 0,
+			.e_x = w.ws_col - 1,
+			.e_y = w.ws_row - 1
+		},
+		.opt = 0,
+		.container = {NULL} 
+	};
+	clear_screen();
+	draw_component(&screen);
 }
